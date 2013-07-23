af39f19b73e8615dcd41d97c2fe735c91c8f7cc0
Malte Swart <mswart@devtation.de>
2013-07-23 22:53:09 +0200

    add tests for status file parsing

---
+++ tests/test_parser.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+from openvpnzone import extract_zones_from_status_file
+
+
+def test_empty_server():
+    assert extract_zones_from_status_file('tests/samples/empty.ovpn-status-v1') \
+        == {}
+
+
+def test_one_client_on_server():
+    assert extract_zones_from_status_file('tests/samples/one.ovpn-status-v1') \
+        == {'one.vpn.example.org': '198.51.100.8'}
+
+
+def test_multiple_client_on_server():
+    assert extract_zones_from_status_file('tests/samples/multiple.ovpn-status-v1') \
+        == {
+            'one.vpn.example.org': '198.51.100.8',
+            'two.vpn.example.org': '198.51.100.12',
+            'three.vpn.example.org': '198.51.100.16'
+        }
+
+
+def test_subnet_for_client():
+    assert extract_zones_from_status_file('tests/samples/subnet.ovpn-status-v1') \
+        == {'one.vpn.example.org': '198.51.100.8'}
